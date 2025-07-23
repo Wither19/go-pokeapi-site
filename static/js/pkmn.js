44ceea30561378dceea51bb70ebc4e9f43d06b68
Joseph Vranicar <josephvranicar1@gmail.com>
2025-07-23 19:37:28 -0400

    c

--- static/js/pkmn.js
+++ static/js/pkmn.js
@@ -9,33 +9,6 @@ function togglePkmnArt() {
 regArt.addEventListener("click", togglePkmnArt);
 shinyArt.addEventListener("click", togglePkmnArt);
 
-// const flavorContainers = Array.from(document.querySelectorAll(".dex-entries > div"));
-// const flavorIds = flavorContainers.map((tag) => tag.id);
-
-// var currentFlavor = 0;
-
-// function cycleFlavor() {
-// 	if (currentFlavor + 1 >= flavorIds.length) {
-// 		currentFlavor = 0;
-// 	} else {
-// 		currentFlavor++;
-// 	}
-
-// 	const hiddenClass = "hidden"
-
-// 	for (let tagID of flavorIds) {
-// 		const flavorElem = document.querySelector(`div#${tagID}`);
-
-// 		if (flavorIds.indexOf(tagID) == currentFlavor) {
-// 			flavorElem.classList.remove(hiddenClass);
-// 		} else {
-// 			flavorElem.classList.add(hiddenClass);
-// 		}
-// 	}
-// }
-
-// // document.getElementById("flavor-cycle-btn").addEventListener("click", cycleFlavor);
-
 function setFlavor(game) {
 	const nonGameElems = document.querySelectorAll(`.dex-entries > div:not(#${game})`);
 	const gameElem = document.querySelector(`div#${game}`);
