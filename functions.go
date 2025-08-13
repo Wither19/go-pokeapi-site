36f264fe53a06c46a7034977069e6d01ced5d887
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-08-13 18:42:24 +0000

    c

--- functions.go
+++ functions.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand/v2"
 	"strconv"
 )
 
 // Adds [length] amount of leading zeroes to [num], for display purposes.
 func leadingZeroes(num int, length int) string {
 	return fmt.Sprintf("%0*d", length, num)
+}
+
+// Random Integer function for use in Go templates, cuz Sprig's randInt suddenly borked.
+func randomNumber(start int, end int) int {
+	return rand.IntN(end) + start
 }
 
 func parseInt(string string) int {
