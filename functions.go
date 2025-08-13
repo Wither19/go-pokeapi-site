5a4614d1f40cfc411c10cd551565f7434eaef486
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-08-13 18:40:46 +0000

    c

--- functions.go
+++ functions.go
@@ -12,11 +12,6 @@ func leadingZeroes(num int, length int) string {
 	return fmt.Sprintf("%0*d", length, num)
 }
 
-// Random Integer function for use in Go templates, cuz Sprig's randInt suddenly borked.
-func randomNumber(start int, end int) int {
-	return rand.IntN(end) + start
-}
-
 func parseInt(string string) int {
 	num, err := strconv.ParseInt(string, 0, 0)
 	if err != nil {
